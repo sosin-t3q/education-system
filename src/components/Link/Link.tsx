1781d00dc11ac1de24ed2a52d299f37b0b955e9d
kimzeze <fridaynight@kakao.com>
2023-06-07 14:43:14 +0900

    [🎨스타일] Link 공통 컴포넌트 마크업 추가

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,25 @@
+import { Link as ReactRouterLink } from 'react-router-dom'
+import styles from './Link.module.css'
+
+interface LinkProps {
+  path: string
+  label: string
+  option: number
+}
+
+const Link = (props: LinkProps) => {
+  const mode =
+    props.option === 1
+      ? styles['link--primary']
+      : props.option === 2
+      ? styles['link--secondary']
+      : styles['link--third']
+
+  return (
+    <ReactRouterLink to={props.path} className={mode}>
+      {props.label}
+    </ReactRouterLink>
+  )
+}
+
+export default Link
