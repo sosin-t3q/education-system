31aa8fb76178bec2ad230c89838a74508eda720e
kimzeze <fridaynight@kakao.com>
2023-06-08 13:37:00 +0900

    [🎸기타] index.ts 폴더 별 파일 생성

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button/Button'
+export { default as Link } from './Link/Link'
+export { default as Title } from './Title/Title'
+export { default as Carousel } from './Carousel/Carousel'
+
+export { default as Badge } from './Badge/Badge'
+export { default as Address } from './Address/Address'
---
+++ src/containers/index.ts
@@ -0,0 +1,1 @@
+export { default as IntroInfo } from './IntroInfo/IntroInfo'
