92fa704b603931b4a5888967c1c19f0ad95b2653
kimzeze <fridaynight@kakao.com>
2023-06-08 16:48:26 +0900

    [🎨스타일] Title 공통 컴포넌트 className 추가, 스타일 수정

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,4 +1,5 @@
 import styles from './Title.module.css'
+console.log(styles.h1)
 
 interface TitleProps {
   type: number
@@ -9,19 +10,33 @@ interface TitleProps {
 const Title = (props: TitleProps) => {
   switch (props.type) {
     case 1:
-      return <h1 className={styles.h1}>{props.label}</h1>
+      return (
+        <h1 className={`${props.className} ${styles.h1}`}>{props.label}</h1>
+      )
     case 2:
-      return <h2 className={styles.h2}>{props.label}</h2>
+      return (
+        <h2 className={`${props.className} ${styles.h2}`}>{props.label}</h2>
+      )
     case 3:
-      return <h3 className={styles.h3}>{props.label}</h3>
+      return (
+        <h3 className={`${props.className} ${styles.h3}`}>{props.label}</h3>
+      )
     case 4:
-      return <h4 className={styles.h4}>{props.label}</h4>
+      return (
+        <h4 className={`${props.className} ${styles.h4}`}>{props.label}</h4>
+      )
     case 5:
-      return <h5 className={styles.h5}>{props.label}</h5>
+      return (
+        <h5 className={`${props.className} ${styles.h5}`}>{props.label}</h5>
+      )
     case 6:
-      return <h6 className={styles.h6}>{props.label}</h6>
+      return (
+        <h6 className={`${props.className} ${styles.h6}`}>{props.label}</h6>
+      )
     default:
-      return <h1 className={styles.h1}>{props.label}</h1>
+      return (
+        <h1 className={`${props.className} ${styles.h1}`}>{props.label}</h1>
+      )
   }
 }
 
