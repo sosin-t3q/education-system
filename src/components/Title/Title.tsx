60121ff4238cef391bdbb4f8ab7f80bc94dd0f83
kimzeze <fridaynight@kakao.com>
2023-06-08 13:37:18 +0900

    [🎨스타일] Title 공통 컴포넌트 마크업 수정

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,26 +1,27 @@
-// import styles from './Title.module.css'
+import styles from './Title.module.css'
 
 interface TitleProps {
   type: number
   label: string
+  className: string
 }
 
-const Title = ({ type, label }: TitleProps) => {
-  switch (type) {
+const Title = (props: TitleProps) => {
+  switch (props.type) {
     case 1:
-      return <h1>{label}</h1>
+      return <h1 className={styles.h1}>{props.label}</h1>
     case 2:
-      return <h2>{label}</h2>
+      return <h2 className={styles.h2}>{props.label}</h2>
     case 3:
-      return <h3>{label}</h3>
+      return <h3 className={styles.h3}>{props.label}</h3>
     case 4:
-      return <h4>{label}</h4>
+      return <h4 className={styles.h4}>{props.label}</h4>
     case 5:
-      return <h5>{label}</h5>
+      return <h5 className={styles.h5}>{props.label}</h5>
     case 6:
-      return <h6>{label}</h6>
+      return <h6 className={styles.h6}>{props.label}</h6>
     default:
-      return <h1>{label}</h1>
+      return <h1 className={styles.h1}>{props.label}</h1>
   }
 }
 
