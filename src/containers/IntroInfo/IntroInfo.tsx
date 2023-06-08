0b1295d39d7e565b86966edf743e0dc520946a2d
kimzeze <fridaynight@kakao.com>
2023-06-08 17:29:38 +0900

    [🎨스타일]: Intro 컨테이너 Text 마크업 수정

--- src/containers/IntroInfo/IntroInfo.tsx
+++ src/containers/IntroInfo/IntroInfo.tsx
@@ -8,10 +8,9 @@ const IntroInfo = () => {
   return (
     <section className="intro-container">
       <Title className="intro-title" type={1} label="AI훈민정음"></Title>
-      <Text
-        variant="subtitle"
-        children="세상에서 인공지능을 가장 잘 활용하는 대한민국"
-      ></Text>
+      <Text variant="subtitle">
+        세상에서 인공지능을 가장 잘 활용하는 대한민국
+      </Text>
     </section>
   )
 }
