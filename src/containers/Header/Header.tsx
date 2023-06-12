89f4956a6fbcae5a6ce5800801beea9959fbb778
hyunzsu <kkm5169@naver.com>
2023-06-12 14:43:57 +0900

    [🔧수정] Header 컴포넌트 css 수정

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -5,14 +5,18 @@ import { ReactComponent as Logo } from '@/assets/Logo.svg'
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Logo className={styles.logo}></Logo>
-      <Link
-        className={`${styles.experience}`}
-        path="/"
-        label="T3Q.ai 체험하기"
-        option={1}
-      />
-      <Link className={`${styles.login}`} path="/" label="로그인" option={2} />
+      <div className={styles['header-inner']}>
+        <Logo className={styles.logo}></Logo>
+        <div className={styles.links}>
+        <Link
+          className={`${styles.experience}`}
+          path="/"
+          label="T3Q.ai 체험하기"
+          option={1}
+        />
+        <Link className={`${styles.login}`} path="/" label="로그인" option={1} />
+        </div>
+      </div>
     </div>
   )
 }
