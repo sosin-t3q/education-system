88a6a367a850463ee3f8c257eccbddb9e16a317c
kimzeze <fridaynight@kakao.com>
2023-06-13 17:20:33 +0900

    [🎨스타일] Carousel 컴포넌트 마크업 및 스타일 수정

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/swiper.min.css'
 import styles from './Carousel.module.css'
+import '@/introSwiper.css'
 
 interface CarouselProps {
   className: string
@@ -19,15 +20,17 @@ const Carousel = ({ className }: CarouselProps) => {
         centeredSlides={true}
         autoplay={{
           delay: 3000,
-          disableOnInteraction: false,
+          disableOnInteraction: true,
         }}
         pagination={{
           clickable: true,
         }}
         modules={[Autoplay, Pagination]}
       >
-        <SwiperSlide>
-          <section className={styles['contents-container']}>
+        <SwiperSlide className="first-slide">
+          <section
+            className={`${styles['contents-container']} ${styles.first}`}
+          >
             <div className={styles['text-contents']}>
               <Title
                 className={styles['contents-title']}
@@ -42,28 +45,30 @@ const Carousel = ({ className }: CarouselProps) => {
                 variant="paragraph"
               >
                 세종대왕께서 글이 없어 자기 뜻을 제대로 표현하지 못하는 백성들을
-                위하여 28글자를 만들어 세상의 모든 소리와 뜻을 전할 수 있게 한
-                것처럼,
+                위하여 <br />
+                28글자를 만들어 세상의 모든 소리와 뜻을 전할 수 있게 한 것처럼,
               </Text>
               <Text
                 className={styles['contents-paragraph']}
                 variant="paragraph"
               >
                 인공지능에서 다루는 데이터 7종, 인공지능이 하는 태스크 4가지를
-                조합한 28가지 우수케이스를 통합 플랫폼에 탑재한 사례집입니다.
+                조합한
+                <br /> 28가지 우수케이스를 통합 플랫폼에 탑재한 사례집입니다.
               </Text>
               <Button
-                className={styles['contents-button']}
+                className={`${styles['contents-button']} ${styles['button-first']}`}
                 option={1}
                 onClick={() => console.log('button click')}
                 label="바로가기"
               ></Button>
             </div>
-            <img src="./src/assets/intro_first.png" />
           </section>
         </SwiperSlide>
-        <SwiperSlide>
-          <section className={styles['contents-container']}>
+        <SwiperSlide className="second-slide">
+          <section
+            className={`${styles['contents-container']} ${styles.second}`}
+          >
             <div className={styles['text-contents']}>
               <Title
                 className={styles['contents-title']}
@@ -78,20 +83,23 @@ const Carousel = ({ className }: CarouselProps) => {
                 variant="paragraph"
               >
                 AI훈민정음 플랫폼은 학교, 군대, 정부, 기업, 의료 등 다양한
-                목적으로 활용가능합니다.
+                목적으로 활용
+                <br />
+                가능합니다.
               </Text>
               <Button
-                className={styles['contents-button']}
+                className={`${styles['contents-button']} ${styles['button-second']}`}
                 option={1}
                 onClick={() => console.log('button click')}
                 label="바로가기"
               ></Button>
             </div>
-            <img src="./src/assets/intro_second.png" />
           </section>
         </SwiperSlide>
-        <SwiperSlide>
-          <section className={styles['contents-container']}>
+        <SwiperSlide className="third-slide">
+          <section
+            className={`${styles['contents-container']} ${styles.third}`}
+          >
             <div className={styles['text-contents']}>
               <Title
                 className={styles['contents-title']}
@@ -105,18 +113,21 @@ const Carousel = ({ className }: CarouselProps) => {
                 className={styles['contents-paragraph']}
                 variant="paragraph"
               >
-                서비스형 인공지능(AIaaS, AI as a Service) 플랫폼은 음식(AI
-                서비스)과 재료(데이터)만 있으면 요리사(개발자 또는 운영자)가
-                원하는 요리를 만들 수 있는 '공유주방'과 같은 개념입니다.
+                서비스형 인공지능(AIaaS, AI as a Service) 플랫폼은
+                음식(AI서비스)과 재료(데
+                <br />
+                이터)만 있으면 요리사(개발자 또는 운영자)가 원하는 요리를 만들수
+                있는 '공유주
+                <br />
+                방'과 같은 개념입니다.
               </Text>
               <Button
-                className={styles['contents-button']}
+                className={`${styles['contents-button']} ${styles['button-third']}`}
                 option={1}
                 onClick={() => console.log('button click')}
                 label="바로가기"
               ></Button>
             </div>
-            <img src="./src/assets/intro_third.png" />
           </section>
         </SwiperSlide>
       </Swiper>
