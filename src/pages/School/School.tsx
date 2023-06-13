19d1df6b7aa9039235f8560f463a8614902fe9c4
rlawlsdn263 <rlawlsdn263@naver.com>
2023-06-13 14:57:11 +0900

    [🎨스타일] School 페이지 스타일 작업

--- src/pages/School/School.tsx
+++ src/pages/School/School.tsx
@@ -1,33 +1,29 @@
 import styles from './School.module.css'
-// import { ReactComponent as Logo } from '@/assets/Logo.svg'
 import { Title, Text, DropdownMenu } from '@/components'
+import { Card } from '@/containers'
 
 const School = () => {
   return (
     <div className={styles.school}>
-      {/* 헤더 */}
-      {/* <nav className={styles.header}>
-        <div className={styles['header-inner']}>
-          <a href="#">
-            <Logo className={styles['header-inner-left']} />
-          </a>
-          <div className={styles['header-inner-right']}>
-            <a href="#" className={styles.link1}>
-              T3Q.ai 체험하기
-            </a>
-            <a href="#" className={styles.link2}>
-              로그인
-            </a>
-          </div>
-        </div>
-      </nav> */}
       {/* 인트로 */}
-      <Title type={2} label="AI훈민정음 서당" className={styles.title} />
-      <Text variant="paragraph" className={styles.subtitle}>
+      <Title type={1} label="AI훈민정음 서당" className={styles.title} />
+      <Text variant="paragraph" className={styles.text}>
         T3Q.ai를 이용한 각 대학별 프로젝트 결과물
       </Text>
       {/* 메뉴 */}
       <DropdownMenu></DropdownMenu>
+      <Title type={2} label="경북대학교" className={styles.subtitle}></Title>
+      <div className={styles['card-container']}>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
     </div>
   )
 }
