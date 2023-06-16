3957d59501b1078e98806faca52a4a9558514f2c
hyunzsu <kkm5169@naver.com>
2023-06-16 11:12:59 +0900

    [🎨스타일] HomeInfo 디자인 수정

--- src/containers/HomeInfo/HomeInfo.tsx
+++ src/containers/HomeInfo/HomeInfo.tsx
@@ -15,17 +15,19 @@ const HomeInfo = () => {
         <iframe
           className={styles.video}
           src="https://www.youtube.com/embed/FF1DBp5EgAU"
-          width="1024"
-          height="576"
-          title="BLACKPINK"
+          width="720"
+          height="405"
+          title="t3q 소개 유튜브 영상"
         ></iframe>
         <Text className={styles.paragraph} variant="paragraph">
-          세종대왕께서 글이 없어 자기 뜻을 제대로 표현하지 못하는 백성들을
-          위하여 28글자를 만들어 세상의 모든 소리와 뜻을
-          <br />
-          전할 수 있게 한 것처럼, AI훈민정음은 인공지능에서 다루는 데이터 7종,
-          인공지능이 하는 태스크 4가지를 조합한 28가
-          <br />지 우수케이스를 통합 플랫폼에 탑재한 사례집 입니다.
+          세종대왕께서 글이 없어 자기 뜻을 제대로 표현하지 못한 백성들을 위하여
+          28글자를 만들어 세상의 모든 소리와 뜻을 전할 수 있게 한 것처럼,
+          인공지능에서 다루는
+          <span style={{ fontWeight: 700 }}>
+            &nbsp; 데이터 7종, 인공지능이 하는 태스크 4가지를 조합한 28가지
+            우수케이스를 통합 플랫폼에 탑재한 사례집
+          </span>
+          입니다.
         </Text>
         <div className={styles['info-data']}>
           <Title
@@ -36,7 +38,7 @@ const HomeInfo = () => {
           <img
             src={imagesData}
             alt="데이터 7종 이미지"
-            className={styles.images}
+            className={`${styles.images} ${styles['images-data']}`}
           />
         </div>
         <Text
@@ -44,8 +46,8 @@ const HomeInfo = () => {
           variant="paragraph"
         >
           인공지능에서 다루는 데이터는
-          <span className={styles.spacer}>
-            텍스트, 이미지, 음성, 영상, 위성, 로그/수치, 바이너리
+          <span style={{ fontWeight: 700 }}>
+            &nbsp; 텍스트, 이미지, 음성, 영상, 위성, 로그/수치, 바이너리&nbsp;
           </span>
           등 7종입니다.
         </Text>
@@ -66,8 +68,10 @@ const HomeInfo = () => {
           variant="paragraph"
         >
           인공지능이 하는 태스크는
-          <span className={styles.spacer}>분류, 회귀, 군집화, 이상감지</span>등
-          4가지입니다.
+          <span style={{ fontWeight: 700 }}>
+            &nbsp;분류, 회귀, 군집화, 이상감지&nbsp;
+          </span>
+          등 4가지입니다.
         </Text>
       </div>
     </div>
