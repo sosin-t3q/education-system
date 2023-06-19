57450c4d773b2031b68482bf0fac95736af5ef1e
chaehaeun <chae_3210@naver.com>
2023-06-19 21:56:08 +0900

    [✨기능] json 로컬 데이터 불러오기

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,19 +1,42 @@
-import { useState } from 'react'
 import { ReactComponent as MathchCase } from '@/assets/match_case.svg'
 import styles from './Input.module.css'
+// import { useState } from 'react'
 
-const Input = () => {
-  const [file, setFile] = useState<string>('')
+interface FileItem {
+  name: string
+  path: string
+  text?: string
+}
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]
-    setFile(file ? file.name : '')
-  }
+interface InputProps {
+  file: FileItem[]
+  type: string
+  selected: string
+}
+
+const Input = ({ file, type, selected }: InputProps) => {
+  const selectedFile = file.filter((item: any) => item.name === selected)[0]
+
+  console.log(selectedFile?.path)
 
   return (
     <div className={styles.selectFile}>
-      <MathchCase />
-      <p>텍스트 파일을 선택해주세요.</p>
+      {selected === 'default' || selected === '예제 선택하기' ? (
+        <>
+          <MathchCase />
+          <p>추론 데이터 파일을 선택주세요</p>
+        </>
+      ) : type === 'text' ? (
+        <p>{selectedFile?.text}</p>
+      ) : type === 'image' ? (
+        <img src={selectedFile?.path} alt={selectedFile?.name} />
+      ) : type === 'audio' ? (
+        <audio controls src={selectedFile?.path} />
+      ) : type === 'video' ? (
+        <video controls src={selectedFile?.path} />
+      ) : (
+        '잘못된 파일 형식입니다.'
+      )}
     </div>
   )
 }
