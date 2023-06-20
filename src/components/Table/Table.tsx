68a3ef8da4739b4db1b6f3b3c01a98ba6ea20901
rlawlsdn263 <rlawlsdn263@naver.com>
2023-06-20 14:31:28 +0900

    [🎨스타일] Table 공용 컴포넌트 생성 및 데이터 입력

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,43 @@
+import TABLE_DATA from '@/data/TABLE_DATA.json'
+import styles from './Table.module.css'
+
+function Table() {
+  const columns = TABLE_DATA[0].columns
+  const data = TABLE_DATA[1].data
+
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          {
+            //기둥의 갯수 + 기둥 제목
+            columns?.map(({ id, title }) => {
+              return (
+                <th key={id} className={styles['top-title']}>
+                  <span>{title}</span>
+                </th>
+              )
+            })
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map(({ id, side, CLF, REG, ANORM, CLS }) => {
+          return (
+            <tr key={id}>
+              <th className={styles['side-title']}>
+                <span>{side}</span>
+              </th>
+              <td>{CLF}</td>
+              <td>{REG}</td>
+              <td>{ANORM}</td>
+              <td>{CLS}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
