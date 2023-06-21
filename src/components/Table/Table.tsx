06e82e7a7fe34e44d8c05f2de177805db427ccf2
rlawlsdn263 <rlawlsdn263@naver.com>
2023-06-21 17:52:10 +0900

    [✨기능] navigate 기능

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,18 +1,30 @@
 import TABLE_DATA from '@/data/TABLE_DATA.json'
 import styles from './Table.module.css'
 import { MouseEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+interface handleNavigateProps {
+  id: number
+}
 
 const Table = () => {
   const title = TABLE_DATA.title
   const columns = TABLE_DATA.content[0].columns
   const data = TABLE_DATA.content[1].data
+  const navigate = useNavigate()
 
-  const handleClick = (e: MouseEvent<HTMLTableElement>) => {
+  // 이벤트 버블링을 막아주는 함수
+  const preventBubbling = (e: MouseEvent<HTMLTableElement>) => {
     e.stopPropagation()
   }
 
+  // naviate 기능을 지닌 함수
+  const handleNavigate = ({ id }: handleNavigateProps) => {
+    navigate(`/detail/${id}`)
+  }
+
   return (
-    <table className={styles.table} onClick={handleClick}>
+    <table className={styles.table} onClick={preventBubbling}>
       <caption className={styles.caption}>{title}</caption>
       <thead>
         <tr>
@@ -35,10 +47,10 @@ const Table = () => {
               <th className={styles['side-title']}>
                 <span className={styles['side-title-span']}>{side}</span>
               </th>
-              <td>{CLF}</td>
-              <td>{REG}</td>
-              <td>{ANORM}</td>
-              <td>{CLS}</td>
+              <td onClick={() => handleNavigate(CLF)}>{CLF.title}</td>
+              <td onClick={() => handleNavigate(REG)}>{REG.title}</td>
+              <td onClick={() => handleNavigate(ANORM)}>{ANORM.title}</td>
+              <td onClick={() => handleNavigate(CLS)}>{CLS.title}</td>
             </tr>
           )
         })}
