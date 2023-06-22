0e9315326b7deddb6b414b16f6ec658375bfa933
chaehaeun <chae_3210@naver.com>
2023-06-22 17:57:28 +0900

    [✨기능] 예제 파일 추가 및 json 수정

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -6,7 +6,7 @@ const Detail = () => {
     <div className={styles.detail}>
       <Header></Header>
       <DetailCarousel className={styles['detail-carousel']}></DetailCarousel>
-      <DetailForm pageId={23}></DetailForm>
+      <DetailForm pageId={20}></DetailForm>
     </div>
   )
 }
