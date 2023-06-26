62c788abe4a55324b4557fb100142ca8055ea5ad
rlawlsdn263 <rlawlsdn263@naver.com>
2023-06-26 21:57:51 +0900

    [🔧수정] 도메인 파라미터 및 경로 수정

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router'
-import { checkUserCookie, createUserCookie, deleteUserCookie } from './index'
-
+import { checkUserCookie } from './index'
+// import { checkUserCookie, createUserCookie, deleteUserCookie } from './index'
 // createUserCookie('user_auth', 'adsadas', 2)
 // deleteUserCookie()
 
@@ -8,7 +8,8 @@ const PrivateRoutes = () => {
   if (checkUserCookie()) {
     return <Outlet></Outlet>
   } else {
-    window.location.href = 'http://www.naver.com'
+    window.location.href =
+      'http://hunmin.demo.t3q.ai:8080/auth/realms/t3q_ai_edu/protocol/openid-connect/auth?client_id=common&response_type=code&redirect_uri=http://hunmin.demo.t3q.ai/page-redirector/signIn-AIHUNMIN&scope=openid&state=BiJHS'
   }
 }
 
