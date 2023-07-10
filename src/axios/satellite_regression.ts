dcb11cf5458365c14e487a657778f12816c28dcf
kimzeze <fridaynight@kakao.com>
2023-07-10 17:25:05 +0900

    [✨기능] 28 case 비동기 통신 함수 생성

---
+++ src/axios/satellite_regression.ts
@@ -0,0 +1,42 @@
+/* 허리케인 위성 사진 풍속 예측 - 위성 회귀 */
+import axios from 'axios'
+import { detailDataAtom, loadingAtom } from '@/atoms'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+import base64DataToFile from './base64DataToFile'
+
+const detailData = useRecoilValue<any>(detailDataAtom)
+const setLoading = useSetRecoilState(loadingAtom)
+
+const satelliteRegression = () => {
+  let data = {
+    file: base64DataToFile(detailData, '사진이름', 'image/jpeg'),
+    url: 'http://dl.idro3vub.aica.t3q.ai/model/api/8c28d/inference',
+  }
+  setLoading(true)
+  axios
+    .post('/inference/file_req_ajx', data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      responseType: 'json',
+    })
+    .then(res => {
+      let json = res.data
+      if (json.res == 'true') {
+        let response_data = json.response.data
+        if (response_data == null) {
+          response_data = json.response.inference
+        }
+      } else {
+        alert('API 호출에 실패했습니다.')
+      }
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+}
+
+export default satelliteRegression
