1c2cddc64e96151882bc97a7202f988573a287e4
kimzeze <fridaynight@kakao.com>
2023-07-11 15:21:32 +0900

    [✨기능] resultAtom 생성

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,4 +1,5 @@
 export { default as modalAtom } from './modalAtom'
 export { default as tableAtom } from './tableAtom'
+export { default as resultAtom } from './resultAtom'
 export { default as loadingAtom } from './loadingAtom'
 export { default as detailDataAtom } from './detailDataAtom'
