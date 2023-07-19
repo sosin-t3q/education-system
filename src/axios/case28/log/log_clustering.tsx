4f1561be5147937279f6284a3ae5ff888c912305
kimzeze <fridaynight@kakao.com>
2023-07-19 10:09:53 +0900

    [✨기능] log 비동기 함수 일부 완성

--- src/axios/case28/log/log_clustering.tsx
+++ src/axios/case28/log/log_clustering.tsx
@@ -1,45 +1,46 @@
 /* 포켓몬 스탯에 따른 군집화 - log 군집화 */
 import axios from 'axios'
-import { detailDataAtom, loadingAtom } from '@/atoms'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { Keyword } from '@/components';
 
-const detailData = useRecoilValue<any>(detailDataAtom)
-const setLoading = useSetRecoilState(loadingAtom)
-
-const logClustering = () => {
-  let data = JSON.stringify({
-    word: detailData.join(),
-    url: 'http://dl.idro3vub.aica.t3q.ai/model/api/8ede6/inference',
-  })
-  setLoading(true)
-
-  let class_info: any = {
+const logClustering = (
+  value: any, // 사용자가 입력한 값 (string or base64)
+  formUrl: any, // 사용자가 입력한 api Url
+  setLoading: any, // 로딩
+  // setResult: any,    // 결과 컴포넌트
+) => {
+  const class_info: any = {
     'physical good, magical bad': '물리 능력치 양호',
     'magical good, physical bad': '특수 능력치 양호',
     'physical good, magical good': '모든 능력치 양호',
     'physical bad, magical bad': '모든 능력치 부족',
   }
 
+  const axiosUrl = '/inference/log_req_ajx' // 고정값
+  /* FormData (apiUrl, data) 형태로 전송 */
+  const formData = new FormData()
+  formData.append('url', formUrl)
+  formData.append('word', value) // 사용자가 전송할 값이 [문자열] 형태일 때
+
+  setLoading(true) // 로딩 표시
+
+  /* axios 비동기 통신 함수 */
   axios
-    .post('/inference/log_req_ajx', data, {
+    .post(axiosUrl, formData, {
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'multipart/form-data',
       },
       responseType: 'json',
     })
     .then(res => {
+      /* response_data에 결과값 반환 */
       let json = res.data
       if (json.res == 'true') {
         let response_data = json.response.data
         if (response_data == null) {
           response_data = json.response.inference
         }
-        response_data = class_info[response_data];
-        <Keyword option={1} label={response_data} />
-        // 결과 들어가는 부분
-        //  $(".result_alert").html(response_data);
-        // $("div.wrap_next").addClass("show_alert_pass");
+        /* 결과값에 따라 결과 컴포넌트 렌더링 */
+        response_data = class_info[response_data]
+        // 결과 컴포넌트
       } else {
         alert('API 호출에 실패했습니다.')
       }
--- src/axios/case28/log/log_regression.tsx
+++ src/axios/case28/log/log_regression.tsx
@@ -1,37 +1,38 @@
 /* NBA 선수 연봉 예측 - log 회귀 */
 import axios from 'axios'
-import { detailDataAtom, loadingAtom } from '@/atoms'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { Keyword } from '@/components'
 
-const detailData = useRecoilValue<any>(detailDataAtom)
-const setLoading = useSetRecoilState(loadingAtom)
+const logRegression = (
+  value: any, // 사용자가 입력한 값 (string or base64)
+  formUrl: any, // 사용자가 입력한 api Url
+  setLoading: any, // 로딩
+  // setResult: any,    // 결과 컴포넌트
+) => {
+  const axiosUrl = '/inference/log_req_ajx' // 고정값
+  /* FormData (apiUrl, data) 형태로 전송 */
+  const formData = new FormData()
+  formData.append('url', formUrl)
+  formData.append('word', value) // 사용자가 전송할 값이 [문자열] 형태일 때
 
-const logRegression = () => {
-  let data = JSON.stringify({
-    word: detailData.join(),
-    url: 'http://dl.idro3vub.aica.t3q.ai/model/api/6dbca/inference',
-  })
-  setLoading(true)
+  setLoading(true) // 로딩 표시
 
+  /* axios 비동기 통신 함수 */
   axios
-    .post('/inference/log_req_ajx', data, {
+    .post(axiosUrl, formData, {
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'multipart/form-data',
       },
       responseType: 'json',
     })
     .then(res => {
+      /* response_data에 결과값 반환 */
       let json = res.data
       if (json.res == 'true') {
         let response_data = json.response.data
         if (response_data == null) {
           response_data = json.response.inference
         }
+        /* 결과값에 따라 결과 컴포넌트 렌더링 */
         // 결과 들어가는 부분
-        //  $(".result_alert").html(response_data);
-        // $("div.wrap_next").addClass("show_alert_pass");
-        <Keyword option={1} label={response_data} />
       } else {
         alert('API 호출에 실패했습니다.')
       }
