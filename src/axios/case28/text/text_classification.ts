f9f724f14e04af157f76b40b2885e19683c77156
chaehaeun <chae_3210@naver.com>
2023-07-31 14:35:34 +0900

    [✨기능] axios 함수 수정

--- src/axios/case28/text/text_classification.ts
+++ src/axios/case28/text/text_classification.ts
@@ -3,13 +3,13 @@
 
 import axios from 'axios'
 
-const textClassification = (
+const textClassification = async (
   value: any, // 사용자가 입력한 값 (text or base64)
   formUrl: any, // 사용자가 입력한 api Url
   setLoading: any, // 로딩
   // setResult: any,    // 결과 컴포넌트
 ) => {
-  const axiosUrl = 'api/inference/text_req_ajx' // 고정값
+  const axiosUrl = 'http://aihunmin-edu.t3q.ai/api/inference/text_req_ajx' // 고정값
   // axiosUrl 이 text 또는 log일 때는 JSON.stringify 형태로 전송
   const jsonData = JSON.stringify({
     word: value,
@@ -19,39 +19,35 @@ const textClassification = (
   setLoading(true) // 로딩 표시
 
   /* axios 비동기 통신 함수 */
-  axios
-    .post(axiosUrl, jsonData, {
+
+  try {
+    const res = await axios.post(axiosUrl, jsonData, {
       headers: {
         'Content-Type': 'application/json',
       },
       responseType: 'json',
     })
-    .then(res => {
-      /* response_data에 결과값 반환 */
-      let json = res.data
-      if (json.res == 'true') {
-        let response_data = json.response.data
-        if (response_data == null) {
-          response_data = json.response.inference
-        }
-        /* 결과값에 따라 결과 컴포넌트 렌더링 */
-        if (response_data == 'pos') {
-          resultData = '긍정'
-          // 긍정 결과 컴포넌트
-        } else {
-          resultData = '부정'
-          // 부정 결과 컴포넌트
-        }
-      } else {
-        alert('API 호출에 실패했습니다.')
+    let json = res.data
+    if (json.res == 'true') {
+      let response_data = json.response.data
+      if (response_data == null) {
+        response_data = json.response.inference
       }
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-    .finally(() => {
-      setLoading(false)
-    })
+      /* 결과값에 따라 결과 컴포넌트 렌더링 */
+      if (response_data == 'pos') {
+        resultData = '긍정'
+        // 긍정 결과 컴포넌트
+      } else {
+        resultData = '부정'
+        // 부정 결과 컴포넌트
+      }
+    }
+  } catch (err) {
+    alert('API 호출에 실패했습니다.')
+  } finally {
+    setLoading(false)
+  }
+
   return { label: resultData }
 }
 
