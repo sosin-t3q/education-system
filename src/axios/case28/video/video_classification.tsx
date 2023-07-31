202437f5c3ed5345c8f730f8d0f54fd9d225ea44
kimzeze <fridaynight@kakao.com>
2023-07-31 17:20:55 +0900

    [🔧수정] video Axios 비동기 처리 방식 변경

--- src/axios/case28/video/video_classification.tsx
+++ src/axios/case28/video/video_classification.tsx
@@ -1,3 +1,4 @@
+/* 비디오 행동 분류 - 영상 분류 */
 import axios from 'axios'
 import base64DataToFile from '../../base64DataToFile'
 
@@ -5,7 +6,7 @@ const videoClassification = async (
   value: any, // 사용자가 입력한 값 (string or base64)
   formUrl: any, // 사용자가 입력한 api Url
   setLoading: any, // 로딩
-  // setResult: any,    // 결과 컴포넌트
+  // setResult: any, // 결과 컴포넌트
 ) => {
   const video_info: any = {
     CricketShot: '크리켓 슛',
@@ -14,45 +15,39 @@ const videoClassification = async (
     ShavingBeard: '면도',
     TennisSwing: '테니스 스윙',
   }
-
-  const axiosUrl = 'api/inference/file_req_ajx' // 고정값
+  const axiosUrl = 'http://aihunmin-edu.t3q.ai/api/inference/file_req_ajx' // 고정값
   const convertData = await base64DataToFile(value, 'video', 'video/mp4')
   /* FormData (apiUrl, data) 형태로 전송 */
   const formData = new FormData()
   formData.append('url', formUrl)
-  formData.append('file', convertData) // 사용자가 전송할 값이 [문자열] 형태일 때
-
+  formData.append('file', convertData)
   let resultData = ''
+
   setLoading(true) // 로딩 표시
 
   /* axios 비동기 통신 함수 */
-  axios
-    .post(axiosUrl, formData, {
+  try {
+    const res = await axios.post(axiosUrl, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
-      responseType: 'json',
+      responseType: 'json', //서버로부터 들어오는 응답값은 JSON 형식
     })
-    .then(res => {
-      let json = res.data
-      if (json.res == 'true') {
-        let response_data = json.response.data
-        if (response_data == null) {
-          response_data = json.response.inference
-        }
-        /* 결과값에 따라 결과 컴포넌트 렌더링 */
-        response_data = video_info[response_data]
-        resultData = response_data
-      } else {
-        alert('API 호출에 실패했습니다.')
+    let json = res.data
+    if (json.res == 'true') {
+      let response_data = json.response.data
+      if (response_data == null) {
+        response_data = json.response.inference
       }
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-    .finally(() => {
-      setLoading(false)
-    })
+      /* 결과값에 따라 결과 컴포넌트 렌더링 */
+      response_data = video_info[response_data]
+      resultData = response_data // 결과값 반환
+    }
+  } catch (err) {
+    alert('API 호출에 실패했습니다.')
+  } finally {
+    setLoading(false)
+  }
   return { label: resultData }
 }
 
