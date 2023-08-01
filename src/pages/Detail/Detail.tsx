1284dd25a0d259ee1029e4d66e02da4865634175
chaehaeun <chae_3210@naver.com>
2023-08-01 10:17:56 +0900

    [🎸기타] api 주소 수정

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -30,7 +30,7 @@ const Detail = () => {
     const getDetailData = async (id: string) => {
       try {
         const response = await axios.get(
-          `http://aihunmin-edu.t3q.ai/api/backend/subpage/${id}`,
+          `http://aihunmin-edu.t3q.ai:8181/api/backend/subpage/${id}`,
         )
         const res = response.data
         if (res['case_data']['data_type'] === 'log') {
