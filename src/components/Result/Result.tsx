be8608ff674b226e30eaba571e58a794d3d7e157
chaehaeun <chae_3210@naver.com>
2023-08-01 12:00:48 +0900

    [🐞버그] 결과값 midi 케이스 추가

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -3,6 +3,7 @@ import styles from './Result.module.css'
 import { ReactComponent as ApprovalDelegation } from '@/assets/approval_delegation.svg'
 import { Keyword } from '@/components'
 import { InferObj } from '@/containers/DetailForm/DetailForm'
+import MidiPlayer from 'react-midi-player'
 
 interface ResultProps {
   infer: string | null | InferObj
@@ -37,17 +38,33 @@ const Result = ({ infer }: ResultProps) => {
           </div>
         )
       case isText && value?.includes('data:image/'): // 이미지
+        if (value.includes('undefined')) {
+          return (
+            <div className={styles['result-cont']}>
+              <p>이미지를 불러올 수 없습니다.</p>
+            </div>
+          )
+        }
         return (
           <div className={styles['result-cont']}>
-            <img src={value} alt="" />
+            <img src={value} alt="결과" />
           </div>
         )
       case isText && value?.includes('data:audio/'): // 오디오
-        return (
-          <div className={styles['result-cont']}>
-            <audio controls src={value} autoPlay={false} />
-          </div>
-        )
+        if (value.includes('audio/midi')) {
+          // 미디 데이터가 들어온 경우
+          return (
+            <div className={styles['result-cont']}>
+              <MidiPlayer src={value} />
+            </div>
+          )
+        } else {
+          return (
+            <div className={styles['result-cont']}>
+              <audio controls src={value} autoPlay={false} />
+            </div>
+          )
+        }
       case isText && value?.includes('data:video/'): // 비디오
         return (
           <div className={styles['result-cont']}>
