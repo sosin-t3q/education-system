559229fbbb00ba713176925bf317089b83b15039
chaehaeun <chae_3210@naver.com>
2023-08-03 15:10:37 +0900

    [✨기능] 비디오 변환 통신 코드 수정

--- src/axios/convertVideo.ts
+++ src/axios/convertVideo.ts
@@ -1,32 +1,27 @@
-import base64DataToFile from '@/axios/base64DataToFile'
 import axios from 'axios'
 
-const convertVideo = async (video: string, fileName: string) => {
-  // 비디오는 base64 형식으로 받아온다
-  const convertData = await base64DataToFile(video, fileName, 'video/avi')
+const convertVideo = async (base64: string) => {
+  const url =
+    'https://cors-anywhere.herokuapp.com/http://aihunmin-edu.t3q.ai:8181/api/converter/video'
 
-  const formData = new FormData()
-  formData.append('file', convertData)
+  const jsonData = JSON.stringify({
+    video_to_base64: base64,
+  })
 
   try {
-    const convertRes = await axios.post(
-      'http://aihunmin-edu.t3q.ai:8181/api/converter/video',
-      formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        responseType: 'json',
+    const res = await axios.post(url, jsonData, {
+      headers: {
+        'Content-Type': 'application/json',
       },
-    )
-    const convertData = convertRes.data
+      responseType: 'json',
+    })
 
-    console.log(convertData)
+    console.log('통신 요청 들어감')
+    const resData = res.data
 
-    // 결과 비디오 src 문자열 반환
-    return 'data:video/mp4;base64,' + convertData
-  } catch (err) {
-    console.log('비디오변환 실패')
+    return 'data:video/mp4;base64,' + resData.data
+  } catch (e) {
+    console.log('변환 실패', e)
   }
 }
 
