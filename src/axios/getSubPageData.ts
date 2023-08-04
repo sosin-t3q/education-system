5e965b8ca0d490a98b8e45236168f1023d6dc341
chaehaeun <chae_3210@naver.com>
2023-08-04 12:00:21 +0900

    [✨기능] 예제 데이터 호출 함수 조건문 switch로 변경

--- src/axios/getSubPageData.ts
+++ src/axios/getSubPageData.ts
@@ -26,20 +26,25 @@ const fetchData = async (
     const res = response.data
     let newData: DataType | null = null
 
-    if (res['case_data']['data_type'] === 'log') {
-      const caseData = res['case_data']['data_list'].map((item: any) => ({
-        ...item,
-        data: logKey(id, item.data),
-      }))
-      newData = { ...res['case_data'], data_list: caseData }
-    } else if (id === '13') {
-      const data = await convertVideo(res['case_data']['data_list'])
-      newData = { ...res['case_data'], data_list: data }
-    } else if (id === '1202') {
-      const transformData = transformPillData(res['case_data']['data_list'])
-      newData = { ...res['case_data'], data_list: transformData }
-    } else {
-      newData = res['case_data']
+    switch (true) {
+      case res['case_data']['data_type'] === 'log':
+        const caseData = res['case_data']['data_list'].map((item: any) => ({
+          ...item,
+          data: logKey(id, item.data),
+        }))
+        newData = { ...res['case_data'], data_list: caseData }
+        break
+      case id === '13':
+        const data = await convertVideo(res['case_data']['data_list'])
+        newData = { ...res['case_data'], data_list: data }
+        break
+      case id === '1202':
+        const transformData = transformPillData(res['case_data']['data_list'])
+        newData = { ...res['case_data'], data_list: transformData }
+
+        break
+      default:
+        newData = res['case_data']
     }
 
     return newData
