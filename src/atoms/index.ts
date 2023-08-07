4d747ac2252bf94eaa2363bfdb75f36e26016eda
kimzeze <fridaynight@kakao.com>
2023-08-07 16:05:12 +0900

    [✨기능] layersView 컴포넌트 상세페이지에서 호출

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -8,7 +8,8 @@ export { default as cartAtom } from './cartAtom'
 export { default as bookAtom } from './bookAtom'
 export { default as resultAtom } from './resultAtom'
 export { default as loadingAtom } from './loadingAtom'
-export { default as visionModalAtom } from "./visionModalAtom"
-export { default as isLoggedInAtom } from "./isLoggedInAtom"
-export { default as userIdAtom } from "./userIdAtom"
-export { default as navigationAtom } from "./navigationAtom"
+export { default as visionModalAtom } from './visionModalAtom'
+export { default as isLoggedInAtom } from './isLoggedInAtom'
+export { default as userIdAtom } from './userIdAtom'
+export { default as navigationAtom } from './navigationAtom'
+export { default as layersAtom } from './layersAtom'
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,10 +1,16 @@
-import { Header, DetailForm, DetailCarousel } from '@/containers'
+import {
+  Header,
+  DetailForm,
+  DetailCarousel,
+  AI28Modal,
+  VisionModal,
+} from '@/containers'
 import { useNavigate, useParams } from 'react-router-dom'
 import styles from './Detail.module.css'
 import { useEffect, useState } from 'react'
-import { Book, Spinner } from '@/components'
+import { Book, LayersView, Spinner } from '@/components'
 import { Helmet } from 'react-helmet-async'
-import { loadingAtom } from '@/atoms'
+import { loadingAtom, modalAtom, visionModalAtom } from '@/atoms'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { getSubPageData } from '@/axios'
 
@@ -22,6 +28,8 @@ const Detail = () => {
   const [data, setData] = useState<DataType | null>(null)
   const loading = useRecoilValue(loadingAtom)
   const setLoading = useSetRecoilState(loadingAtom)
+  const modal = useRecoilValue(modalAtom)
+  const visionModal = useRecoilValue(visionModalAtom)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -48,13 +56,16 @@ const Detail = () => {
       </Helmet>
       <Header />
       <main className={styles.detailMain}>
-        <Book className={styles['detail-book']} />
+        <Book className={styles['detail-view']} />
+        <LayersView className={styles['detail-book']} />
         <DetailCarousel
           pageId={id}
           className={styles['detail-carousel']}
         ></DetailCarousel>
         <DetailForm data={data} pageId={id} />
       </main>
+      {modal && <AI28Modal />}
+      {visionModal && <VisionModal />}
       {loading && <Spinner />}
     </div>
   )
