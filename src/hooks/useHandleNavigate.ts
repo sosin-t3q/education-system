3072bac2aafec26e4814c2e67d6bff5d31256c07
김진우 <rlawlsdn263@naver.com>
2023-08-19 12:12:04 +0900

    [✨기능] 제한된 접근기능 커스텀훅 생성

--- src/hooks/useHandleNavigate.ts
+++ src/hooks/useHandleNavigate.ts
@@ -1,22 +1,26 @@
-import Cookies from 'js-cookie'
+Cookies
 import { useNavigate } from 'react-router-dom'
 import { useKeycloak } from '@react-keycloak/web'
 import { useSetRecoilState, SetterOrUpdater } from 'recoil'
 import { loadingAtom } from '@/atoms'
+import Cookies from 'js-cookie'
 
 const useHandleNavigate = () => {
   const setLoading = useSetRecoilState(loadingAtom)
 
   const navigate = useNavigate()
   const { keycloak } = useKeycloak()
-  const userAuth = Cookies.get("user_auth")
   
   const checkAuthNavigation = async (id: number, setModal: SetterOrUpdater<boolean>) => {
     setLoading(true)
-    setModal(false)
+    if (setModal) {
+      // setModal을 옵셔널로 줬기 때문에 undefined가 들어올 수도 있다.
+      // 그렇기 때문에 setModal이 있을 때만 실행하게끔 해준다.
+      setModal(false);
+    }
 
-    //쿠키에 userAuth가 없을 경우
-    if (!userAuth) {
+    //키클락 인증이 안 된 경우(로그아웃 상태)
+    if (!keycloak.authenticated) {
       try {
         // 키클락 로그인 페이지로 이동시킨 후 로그인 성공 시에 접근하려던 상세페이지로 이동
         await keycloak.login({
@@ -28,7 +32,7 @@ const useHandleNavigate = () => {
         setLoading(false)
       }
     } else {
-      //쿠키에 userAuth가 있을 경우에는 상세페이지로 바로 이동
+      //키클락 인증이 된 경우(로그인 상태)
       navigate(`/detail/${id}`)
       setLoading(false)
     }
