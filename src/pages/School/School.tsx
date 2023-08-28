84c5e839a2a5b54a3b6f3e76c957d1db06cf2318
hyunzsu <kkm5169@naver.com>
2023-08-28 11:27:32 +0900

    [🔧수정] School 페이지 세션스토리지 추가

--- src/pages/School/School.tsx
+++ src/pages/School/School.tsx
@@ -17,13 +17,12 @@ interface SchoolType {
 
 interface SchoolData {
   [schoolName: string]: {
-    color: string;
-    title: string;
-  };
+    color: string
+    title: string
+  }
 }
 
 const School = () => {
-
   const schoolData: SchoolData = {
     경북대학교: {
       color: 'kyungbuk',
@@ -36,26 +35,48 @@ const School = () => {
     // 필요한 경우 더 많은 학교, 색상 및 제목을 추가합니다
   }
 
-  const [selectedSchool, setSelectedSchool] = useState<'경북대학교' | '고려대학교'>('경북대학교')
-  const checkAuthNavigation = useHandleNavigate()
-  const setIsModalOpen = useSetRecoilState(isModalOpenAtom);
+  const initialSelectedSchool =
+    sessionStorage.getItem('selectedSchool') || '경북대학교'
 
+  const [selectedSchool, setSelectedSchool] = useState<
+    '경북대학교' | '고려대학교'
+  >(initialSelectedSchool)
+  const checkAuthNavigation = useHandleNavigate()
+  const setIsModalOpen = useSetRecoilState(isModalOpenAtom)
 
   const handleSchoolChange = (school: string) => {
-    if (schoolData[school as '경북대학교' | '고려대학교'])  {
+    if (schoolData[school as '경북대학교' | '고려대학교']) {
       setSelectedSchool(school as '경북대학교' | '고려대학교')
+      sessionStorage.setItem('selectedSchool', school) // 선택한 학교를 세션 스토리지에 저장
     }
   }
+
+  useEffect(() => {
+    console.log(selectedSchool)
+  }, [selectedSchool])
+
+  // popstate 이벤트(뒤로/앞으로 탐색)를 감지합니다.
+  useEffect(() => {
+    const handlePopState = () => {
+      const storedSchool =
+        sessionStorage.getItem('selectedSchool') || '경북대학교'
+      setSelectedSchool(storedSchool)
+    }
+
+    window.addEventListener('popstate', handlePopState)
+    return () => {
+      window.removeEventListener('popstate', handlePopState)
+    }
+  }, [])
 
   const selectedSchoolData = schoolData[selectedSchool]
   const cardColor = selectedSchoolData.color
   const pageTitle = selectedSchoolData.title
-
   const selectedSchoolCards = schoolsData.schools[selectedSchool] || []
 
   //서당페이지에서 레이아웃을 클릭할 경우 스크롤이 사라지는 걸 방지하기 위한 방법
   useEffect(() => {
-    setIsModalOpen(false);
+    setIsModalOpen(false)
   }, [])
 
   return (
@@ -92,8 +113,7 @@ const School = () => {
               cardColor={cardColor}
               onClickCard={() => {
                 checkAuthNavigation(school.id)
-                }
-              }
+              }}
             />
           ))}
         </div>
