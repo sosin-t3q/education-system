a508584b794f915e6f1b7ee149a0c67b433e83c6
kimzeze <fridaynight@kakao.com>
2023-09-01 16:57:44 +0900

    [🎸기타] 라인 위치 변경 및 주석 수정

--- src/axios/combinedProcessor.ts
+++ src/axios/combinedProcessor.ts
@@ -1,5 +1,3 @@
-import { SetterOrUpdater } from 'recoil'
-import { CancelTokenSource } from 'axios'
 import {
   audioProcessor,
   binaryProcessor,
@@ -11,6 +9,8 @@ import {
 } from '@/axios/case28'
 import { visionProcessor } from '@/axios/vision'
 import { schoolProcessor } from '@/axios/school'
+import { SetterOrUpdater } from 'recoil'
+import { CancelTokenSource } from 'axios'
 
 const combinedProcessor = (
   id: string | undefined,
@@ -32,11 +32,11 @@ const combinedProcessor = (
   let funcIndex = 0
   let taskIndex: number
   let targetTask: string
-  const targetId = +id // string으로 받아온 id 값을 number타입으로 변환하기 위함
+  const targetId = id ? +id : 0
 
   switch (true) {
+    /* 훈민정음 예제일 때 (id 1~28) */
     case targetId >= 1 && targetId <= 28: {
-      // 훈민정음 예제일 때 (id 1~28)
       const tasks = ['classification', 'regression', 'anomaly', 'clustering']
       funcIndex = Math.floor((targetId - 1) / 4)
       taskIndex = (targetId - 1) % 4
@@ -52,8 +52,9 @@ const combinedProcessor = (
         source,
       )
     }
+
+    /* 비전 예제일 때 (id 101~199) */
     case targetId > 100 && targetId < 200: {
-      // 비전 예제일 때 (id 101~199)
       return visionProcessor(
         targetId,
         value,
@@ -64,6 +65,7 @@ const combinedProcessor = (
       )
     }
 
+    /* 학교 예제일 때 (id 1100~1299) */
     case targetId >= 1100 && targetId < 1300: {
       return schoolProcessor(
         targetId,
@@ -75,6 +77,7 @@ const combinedProcessor = (
       )
     }
 
+    /* 정해지지않은 ID값이 들어오거나 ID값 호출이 잘못되었을 때 */
     default:
       // eslint-disable-next-line no-console
       console.log(
